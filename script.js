98a79027ef103943eebd6fa843ef06f9c5e0ee72
Pénélope <lu.obrad@gmail.com>
2024-07-02 18:40:04 +0200

    whatyearisit-frontend modifié après déploiement

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ fetch('https://whatyearisit-backend-ebon.vercel.app/year')
    document.querySelector('#year').textContent = data.year;
  });
 
+ fetch('https://whatyearisit-frontend-o2wf.vercel.app/year')
+ .then(response => response.json())
+ .then(data => {
+   document.querySelector('#year').textContent = data.year;
+ });
